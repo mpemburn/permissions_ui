2c8d18323b152ecf01a11ecddc8987c178727b69
Mark Pemburn <mark@pemburn.com>
2021-02-23 09:47:30 -0500

    Blade tweaks.

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -4,7 +4,7 @@
             {{ __('Permissions') }}
         </h2>
     </x-slot>
-    <div id="acl_wrapper" data-context="permission" class="report">
+    <div id="acl_wrapper" data-context="permission" class="permissions-table">
         <div class="controls">
             <button id="edit_permission" class="modal-open rounded px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
                 Add Permission
@@ -12,19 +12,19 @@
         </div>
         <table id="permission-table" class="stripe">
             <thead>
-                <th>
-                    Permission Name
-                </th>
-                <th>
+            <th>
+                Permission Name
+            </th>
+            <th>
 
-                </th>
+            </th>
             </thead>
             @foreach ($permissions as $permission)
                 <tr class="" id="{!! $permission->id !!}" data-name="{!! $permission->name !!}">
                     <td>
                         {!! $permission->name !!}
                     </td>
-                    <td class="dt-right">
+                    <td class="text-right">
                         <button data-edit="{!! $permission->id !!}" class="w-20 ml-3 rounded px-3 py-1 bg-green-300 hover:bg-green-700 hover:text-white focus:shadow-outline focus:outline-none">
                             Edit
                         </button>
@@ -39,7 +39,7 @@
     </div>
     <div class="permission-modal">
         <!--Modal-->
-        <div class="permui-edit modal opacity-0 pointer-events-none w-full h-full top-0 left-0 flex items-center justify-center">
+        <div class="modal opacity-0 pointer-events-none w-full h-full top-0 left-0 flex items-center justify-center">
             <div class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
             @include('components.token-form')
             @include('permissions.edit')
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -4,13 +4,13 @@
             {{ __('Roles') }}
         </h2>
     </x-slot>
-    <div id="acl_wrapper" data-context="role" class="report">
+    <div id="acl_wrapper" data-context="role" class="roles-table">
         <div class="controls">
             <button id="edit_role" class="modal-open rounded px-3 py-1 bg-blue-700 hover:bg-blue-500 text-white focus:shadow-outline focus:outline-none">
                 Add Role
             </button>
         </div>
-        <table id="role-table" class="stripe">
+        <table id="role-table">
             <thead>
             <th>
                 Role Name
@@ -34,7 +34,7 @@
                             @endforeach
                         </ul>
                     </td>
-                    <td class="dt-right">
+                    <td class="text-right">
                         <button data-edit="{!! $role->id !!}" class="w-20 ml-3 rounded px-3 py-1 bg-green-300 hover:bg-green-700 hover:text-white focus:shadow-outline focus:outline-none">
                             Edit
                         </button>
@@ -57,7 +57,6 @@
         <div>
             @include('components.confirmation-dialog')
         </div>
-    </div>
     </div>
 
 </x-app-layout>
